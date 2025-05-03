d03b046391a957a05176b35542e0cb301c8d985d
CelestialMist7 <krishkorat33@gmail.com>
2025-05-03 09:52:06 +0530

    homepage done

---
+++ Lecture_8/homepage/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#alertButton").addEventListener("click", function () {
+      alert("Thanks for visiting my homepage!");
+  });
+});
